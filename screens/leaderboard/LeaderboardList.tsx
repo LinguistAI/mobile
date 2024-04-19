07f171bed5b9c2a125ec453d6b0f55e101f12e06
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-04-19 23:22:55 +0300

    feat(LINGUIST-58): Added buttons for pagination, updated UI

--- screens/leaderboard/LeaderboardList.tsx
+++ screens/leaderboard/LeaderboardList.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { FlatList, RefreshControl, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Dimensions, FlatList, RefreshControl, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import ShimmerPlaceholder from 'react-native-shimmer-placeholder';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,25 +9,71 @@ import CenteredFeedback from '../../components/common/CenteredFeedback';
 import Colors from '../../theme/colors';
 import LeaderboardUserCard from './LeaderboardUserCard';
 import useUser from '../../hooks/useUser';
+import ActionButton from '../../components/common/ActionButton';
 
 const DEFAULT_PAGE = 0;
 const DEFAULT_PAGE_SIZE = 15;
 
 const LeaderboardList = () => {
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+  const [totalPageNum, setTotalPageNum] = useState(0);
   const [trigger, { data: leaderboard, isLoading, isError, isFetching }, lastPromiseInfo] =
     useLazyGetGlobalLeaderboardQuery();
   const { user } = useUser();
+  const flatListRef = useRef<FlatList<any>>(null);
+  const [rowHeights, setRowHeights] = useState<number[]>([]);
+  const [numVisibleRows, setNumVisibleRows] = useState<number>(0);
+
+  const goToPreviousPage = () => {
+    if (currentPage > 0) {
+      // setCurrentPage((prevPage) => prevPage - 1);
+      trigger({ size: DEFAULT_PAGE_SIZE, page: currentPage - 1 });
+    }
+  };
+
+  const goToNextPage = () => {
+    // setCurrentPage((prevPage) => prevPage + 1);
+    if (currentPage < totalPageNum - 1) {
+      trigger({ size: DEFAULT_PAGE_SIZE, page: currentPage + 1 });
+    }
+  };
 
   const onRefresh = useCallback(async () => {
     setIsRefreshing(true);
-    await trigger({ size: DEFAULT_PAGE_SIZE });
+    await trigger({ size: DEFAULT_PAGE_SIZE, page: currentPage });
     setIsRefreshing(false);
   }, []);
 
   useEffect(() => {
     trigger({ size: DEFAULT_PAGE_SIZE });
+    // setCurrentPage(leaderboard!.currentPage);
   }, []);
+
+  useEffect(() => {
+    const screenHeight = Dimensions.get('window').height - 100;
+    const calculatedNumVisibleRows = Math.floor(screenHeight / getItemLayout(0, 0).length);
+    setNumVisibleRows(calculatedNumVisibleRows);
+  }, []);
+
+  useEffect(() => {
+    if (leaderboard) {
+      setCurrentPage(leaderboard.currentPage);
+      setTotalPageNum(leaderboard.totalPages);
+
+      console.log('curretn:', currentPage);
+      console.log('total:', totalPageNum);
+    }
+    if (leaderboard && flatListRef.current) {
+      const loggedUserIndex = leaderboard.xprankings.findIndex((item) => item.user.username === user.username);
+
+      if (loggedUserIndex !== -1) {
+        // Calculate the target index to scroll to (approximately the middle of the screen)
+        let targetIndex = Math.max(loggedUserIndex - Math.floor(numVisibleRows / 2), 0);
+        flatListRef.current.scrollToIndex({ animated: true, index: targetIndex });
+      }
+    }
+  }, [leaderboard, currentPage]);
 
   const renderSkeletonList = () => {
     return (
@@ -37,6 +83,24 @@ const LeaderboardList = () => {
         ))}
       </View>
     );
+  };
+
+  const getItemLayout = (_: any, index: number) => {
+    if (index >= rowHeights.length) {
+      return { length: 0, offset: 0, index };
+    }
+
+    // Calculate the offset by summing the heights of all items before the current index
+    const offset = rowHeights.slice(0, index).reduce((acc, height) => acc + height, 0);
+    return { length: rowHeights[index], offset, index };
+  };
+
+  const measureRowHeight = (index: number, height: number) => {
+    setRowHeights((prevRowHeights) => {
+      const newHeights = [...prevRowHeights];
+      newHeights[index] = height;
+      return newHeights;
+    });
   };
 
   if (isLoading) {
@@ -49,11 +113,32 @@ const LeaderboardList = () => {
 
   return (
     <View style={styles.root}>
+      {totalPageNum > 1 ? (
+        <View style={styles.pageButtons}>
+          <TouchableOpacity onPress={goToPreviousPage} disabled={currentPage === 0}>
+            <Ionicons name="chevron-back-circle-outline" size={32} color={currentPage === 0 ? 'gray' : 'black'} />
+          </TouchableOpacity>
+          <Text style={{ fontSize: 18, marginHorizontal: 15 }}>{currentPage + 1}</Text>
+          <TouchableOpacity onPress={goToNextPage} disabled={currentPage === totalPageNum - 1}>
+            <Ionicons
+              name="chevron-forward-circle-outline"
+              size={32}
+              color={currentPage === totalPageNum - 1 ? 'gray' : 'black'}
+            />
+          </TouchableOpacity>
+        </View>
+      ) : null}
+
       <FlatList
+        ref={flatListRef}
         numColumns={1}
         contentContainerStyle={styles.friendsListStyle}
         data={leaderboard.xprankings}
-        renderItem={({ item, index }) => <LeaderboardUserCard leaderboardUser={item} loggedInUser={user.username} />}
+        renderItem={({ item, index }) => (
+          <View onLayout={(event) => measureRowHeight(index, event.nativeEvent.layout.height)}>
+            <LeaderboardUserCard leaderboardUser={item} loggedInUser={user.username} />
+          </View>
+        )}
         ListEmptyComponent={
           <View style={{ height: '80%', display: 'flex', justifyContent: 'center' }}>
             <CenteredFeedback
@@ -63,13 +148,16 @@ const LeaderboardList = () => {
           </View>
         }
         refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
+        getItemLayout={getItemLayout}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {},
+  root: {
+    marginBottom: 100,
+  },
   friendsListStyle: {
     padding: 10,
   },
@@ -93,6 +181,15 @@ const styles = StyleSheet.create({
     height: 75,
     alignSelf: 'center',
     marginBottom: 10,
+  },
+  pageButtons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 5,
+    width: '99%',
+    alignSelf: 'center',
+    marginTop: 15,
   },
 });
 
--- screens/leaderboard/LeaderboardUserCard.tsx
+++ screens/leaderboard/LeaderboardUserCard.tsx
@@ -1,27 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { RFriendship, RLeaderboardUser } from '../../components/user/types';
 import React from 'react';
 import Card from '../../components/common/Card';
 import Colors from '../../theme/colors';
-import { User } from '../../types';
 
 interface LeaderboardUserCardProps {
   leaderboardUser: RLeaderboardUser;
   loggedInUser: string;
 }
 
+const space = '   ';
+
 const LeaderboardUserCard = ({ leaderboardUser, loggedInUser }: LeaderboardUserCardProps) => {
   const { user, experience: xp, ranking } = leaderboardUser;
+
+  const renderRankingImage = () => {
+    if (ranking === 1) {
+      return require('../../assets/rank_icons/first.png');
+    } else if (ranking === 2) {
+      return require('../../assets/rank_icons/first.png');
+    } else if (ranking === 3) {
+      return require('../../assets/rank_icons/first.png');
+    } else {
+      return null;
+    }
+  };
 
   return (
     <Card style={user.username === loggedInUser ? styles.highlightedCell : styles.contentRoot}>
       <View>
         <View style={styles.mainInfoContainer}>
-          <Text style={styles.mainInfo}>{ranking}. </Text>
+          <View style={styles.mainInfoContainer}>
+            {ranking <= 3 && <Image source={renderRankingImage()} style={styles.rankImage} />}
+            {ranking > 3 && <Text style={styles.rankingInfo}>{ranking}.</Text>}
 
-          <Text style={styles.mainInfo}>{user.username}</Text>
-          <Text style={styles.mainInfo}>{xp}</Text>
+            <Text style={styles.mainInfo}>{user.username}</Text>
+          </View>
+          <Text style={styles.subinfo}>{xp} XP</Text>
         </View>
       </View>
     </Card>
@@ -50,10 +66,18 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'row',
   },
+  rankingInfo: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: Colors.purple[500],
+    marginLeft: 10,
+    marginRight: 10,
+  },
   mainInfo: {
     fontWeight: 'bold',
     fontSize: 16,
-    color: Colors.primary[500],
+    color: Colors.purple[500],
+    marginRight: 5,
   },
   subinfo: {
     color: Colors.gray[500],
@@ -64,8 +88,14 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     width: '96%',
     borderRadius: 0,
-    borderWidth: 1,
-    borderColor: 'black',
+    // borderWidth: 1,
+    // borderColor: 'black',
+    backgroundColor: '#F3E7FF',
+  },
+  rankImage: {
+    width: 30,
+    height: 30,
+    marginRight: 5,
   },
 });
 

ce25bed724e1856319fb742054f528aec757e476
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2024-04-23 02:49:47 +0300

    fix(LINGUIST-423): Fixed status update during sending friendship request

--- components/common/Text.tsx
+++ components/common/Text.tsx
@@ -16,16 +16,16 @@ const LText = ({ children, style, centered, marginHorizontal = 0, size, onPress 
   let currentStyle = styles.titleTextCustom;
   let mergedStyle = StyleSheet.compose(currentStyle, style as TextStyle);
 
+  // if the custom style includes font weight bold, remove it and set the bold version of the default font
   if (style && (style as TextStyle).fontWeight === 'bold') {
     const { fontWeight, ...rest } = style as TextStyle;
     mergedStyle = StyleSheet.compose(styles.titleTextCustomBolder, rest);
   }
 
+  // if size prop is seperetaly set, override other font size values
   if (size) {
     mergedStyle = StyleSheet.compose(mergedStyle, { fontSize: size });
   }
-
-  console.log('aloo', mergedStyle);
 
   const content = (
     <Text style={[mergedStyle, { textAlign, marginHorizontal: marginHorizontal }]}>{children}</Text>
